b760c80843df3cb264f54aaae2ee332c3c452d07
daohuong2004 <126840583+daohuong2004@users.noreply.github.com>
2023-10-18 23:52:39 +0700

    :<

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module com.example.oopproject {
     requires javafx.fxml;
     requires java.sql;
     requires org.jsoup;
+    requires some.other.module; // other module dependencies
+    requires javazoom.jl.player; // add this line
+
 
 
     opens com.example.oopproject to javafx.fxml;
